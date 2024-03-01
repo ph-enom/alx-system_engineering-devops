0f185efdadd275fca773ffd2afadf70cdf0adb77
patrickmuchiri <pmuchirig@gmail.com>
2024-02-29 18:06:10 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 #!/usr/bin/pup
-# Ensure that pip3 is installed
 package { 'python3-pip':
   ensure => installed,
 }
 
-# Install Flask version 2.1.0 using pip3
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
+
